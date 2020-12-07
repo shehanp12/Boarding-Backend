b8200db4dc2df4f6d2e817e89dc6badfd85cae3b
shehanp12 <shehanp068@gmail.com>
2020-12-07 21:59:30 +0530

    mongooose

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const router = express.Router();
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Imports Routes
+const authRoute = require('./routes/auth');
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//Route Middleware
+app.use('/api/user',authRoute);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(5000,() => console.log('Server is running'))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.post('/register',(req,res) =>{
+    res.send('Register');
+});
 
 
 module.exports=router;
