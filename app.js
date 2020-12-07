c9371317831fe94f3db04058203dcdebf29b9660
shehanp12 <shehanp068@gmail.com>
2020-12-08 01:12:23 +0530

    app

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+const  mongoose = require('mongoose');
+const dotenv= require('dotenv');
+
+dotenv.config();
+
+//connect to database
+mongoose.connect(process.env.DB_CONNECT , { useNewUrlParser : true },
+    () => console.log('connected to db'));
+
 
 //Imports Routes
 const authRoute = require('./routes/auth');
